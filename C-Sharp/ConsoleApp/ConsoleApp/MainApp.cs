be8be52bb9b104ec7dd0603f401d3890b65dc605
Juan Jose Cuadros <restrepojuanjose8@gmail.com>
2022-11-08 13:22:20 -0500

    firt commit learn languages

---
+++ C-Sharp/ConsoleApp/ConsoleApp/MainApp.cs
@@ -0,0 +1,6 @@
+﻿// version 10 de c sharp
+Console.Write("Escribe tu nombre: ");
+var name = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Hola tu nombre es: " +  name);
+Console.WriteLine("La hora actual es: " + DateTime.Now.ToString());
